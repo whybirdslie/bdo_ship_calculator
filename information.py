74266e986ffcaa71fc5910b01509a243df1947fc
whybirdslie <jeffreycheung2000@hotmail.com>
2024-10-17 18:49:58 +0200

    german version done

--- information.py
+++ information.py
@@ -1,10 +1,22 @@
 from browser.html import *
-from translations_de import info_de
+from translations_de import info_de, static_text_de
 
 def translate(text, language):
     if language == 'de':
         return info_de.get(text, text)
     return text
+
+def translate_ship_stat(stat, value, language):
+    if language == 'de':
+        if 'slots' in value:
+            value = value.replace('slots', info_de.get('slots', 'slots'))
+        if 'per side(player)' in value:
+            value = value.replace('per side(player)', info_de.get('per side(player)', 'per side(player)'))
+        if 'per side(captain)' in value:
+            value = value.replace('per side(captain)', info_de.get('per side(captain)', 'per side(captain)'))
+        if value.endswith('s'):
+            value = value[:-1] + info_de.get('s', 's')
+    return value
 
 def gen_info(language='en'):
     shipstats = {
@@ -154,7 +166,7 @@ def gen_info(language='en'):
         ["Panokseon"]
     ]
     ret = P(translate("There is more than 1 way to get an Epheria Sailboat/Frigate. This page shows the upgrade route but you can also purchase it from the CM, craft it directly, or exchange [Event] Radiant Shakatu's Seal x20 for it.", language))
-    ret += P(translate("More information can be found", language) + " " + A(translate("at this spreadsheet", language), href="https://docs.google.com/document/d/1basknMfrfcH6AzJD9PkzeUunqrIGTuS6SfXPf3a7pso/preview", target="_blank") + " " + translate("or", language) + " " + A(translate("these patch notes", language), href="https://www.blackdesertonline.com/news/view/3216", target="_blank"))
+    ret += P(translate("More information can be found", language) + " " + A(translate("at this spreadsheet", language), href="https://docs.google.com/document/d/1basknMfrfcH6AzJD9PkzeUunqrIGTuS6SfXPf3a7pso/preview", target="_blank") + ".")
     ret += P(translate("Barter items that you can trade for ship parts unlock as you finish more trades. You can always trade t1 barter items for verdant stone coupon though.", language))
     ret += P(translate("Ship parts used for upgrade need to be full durability.", language))
     ret += P(translate("All sea monsters can drop parts for upgrading ships. There is no list of which drops are where yet. (Nov-6)", language))
@@ -171,7 +183,8 @@ def gen_info(language='en'):
         for key in shipstats[table[0]]:
             tr = TR(TD(translate(key, language)))
             for ship in table:
-                tr <= TD(shipstats[ship][key])
+                translated_value = translate_ship_stat(key, shipstats[ship][key], language)
+                tr <= TD(translated_value)
             t <= tr
 
         ret += t + BR()
